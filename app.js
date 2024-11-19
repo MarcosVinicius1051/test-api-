b4e8b1eddebf05aec3a2ecdf3adb71ac97db16ab
MarcosVinicius1051 <marcosluxvinicius@gmail.com>
2024-11-19 16:18:09 -0300

    add cors dnv

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 import produtosAvo from "./produtos.js";
 import express from "express";
-// import cors from "cors";
+import cors from "cors";
 
-// app.use(cors());
+app.use(cors());
 const app = express();
 
 app.get('/produtos', (req, res) => {
     res.status(200).send(produtosAvo);
 });
 
-app.listen(3000,()=>{console.log("rodando")});
+app.listen(3000,()=>{console.log("rodando....")});
