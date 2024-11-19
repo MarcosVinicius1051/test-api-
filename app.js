a561d609c468fd440d80fce79877cca78a6ba45b
MarcosVinicius1051 <marcosluxvinicius@gmail.com>
2024-11-19 16:02:10 -0300

    tentando dnv

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ import express from "express";
 
 const app = express();
 
-app.get('/produtos',(req,res) =>{
-    res.status(200).send(produtosAvo)
-})
+app.get('/produtos', (req, res) => {
+    res.status(200).send(produtosAvo);
+});
 
 app.listen(3000,()=>{console.log("rodando")});
