123e449a2a20e670b7b4746991bf2bf91ccfb0b7
MarcosVinicius1051 <marcosluxvinicius@gmail.com>
2024-11-19 16:05:02 -0300

    install cors

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import produtosAvo from "./produtos.js";
 import express from "express";
+import cors from "cors";
 
+app.use(cors());
 const app = express();
 
 app.get('/produtos', (req, res) => {
