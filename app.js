e3fb20fdbd8c53e073628853133d9b5ae28c043d
MarcosVinicius1051 <marcosluxvinicius@gmail.com>
2024-11-19 10:50:55 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+import produtosAvo from "./produtos.js";
+import express from "express";
+
+const app = express();
+
+app.get('/produtos',(req,res) =>{
+    res.status(200).send(produtosAvo)
+})
+
+app.listen(3000);
