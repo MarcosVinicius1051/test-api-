887689dc00f7525f33db02b41c390d8752eda118
MarcosVinicius1051 <marcosluxvinicius@gmail.com>
2024-11-19 16:37:45 -0300

    nova imagen

--- produtos.js
+++ produtos.js
@@ -1,7 +1,8 @@
 const produtosAvo =[
     {id:1, nome: "roupa azul",img:"https://loja.comerciomix.com.br/media/catalog/product/cache/fb4f878514d02efd710032ded901d118/c/a/camiseta-azul-royal-para-sublima_o-tradicional_1.jpg"},
     {id:2, nome: "roupa verde", img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLZRuG6QIgAYSTSzKCIjvb0735D31n_FwBtw&s"},
-    {id:3, nome: "roupa amarela", img:"https://images.tcdn.com.br/img/img_prod/275189/camisa_amarela_ouro_100_poliester_para_sublimacao_p_3603_1_d9fb23a5c9d5eee642322649344448a5.jpg"}
+    {id:3, nome: "roupa amarela", img:"https://images.tcdn.com.br/img/img_prod/275189/camisa_amarela_ouro_100_poliester_para_sublimacao_p_3603_1_d9fb23a5c9d5eee642322649344448a5.jpg"},
+    {id:4, nome: "roupa roxa", img: "/imagens/ff132a2096.png"}
 ]
 
 export default produtosAvo
